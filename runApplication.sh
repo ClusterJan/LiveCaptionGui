5c5f8911e6e65c1734b33cf22ede3bf59f37eab5
Jan von Aschwege <jan@aschwege.me>
2023-02-05 17:28:24 +0100

    Add sleep

--- runApplication.sh
+++ runApplication.sh
@@ -4,6 +4,8 @@
 tmux new -d -s my-session-name \; split-window -v ;\
 tmux send-keys -t my-session-name.0 "tail -f live-translation/captions.txt" ENTER
 tmux send-keys -t my-session-name.1 "export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/key.json && cd live-translation && poetry shell " ENTER
+
+sleep 2
 tmux send-keys -t my-session-name.1 "translate -f it-IT -t en-US" ENTER
 
 # Use this to connect whenever you want
